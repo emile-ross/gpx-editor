19b25a0cba2ce48542b33dd64800ed86eec13e65
emile-ross <emileross@tutanota.de>
2026-06-04 17:10:15 -0400

    feat: create main.c
    add argument parsing and error messages

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	if (!(argc > 1))
+	{
+		err("Missing arguments");
+	}
+}
+
+
+void err(const char *error_message)
+{
+	printf("\x1b[31m%s\x1B[0m\n", error_message);
+	if (error_message == NULL)
+	{
+		exit(1);
+	}
+}
