4a37035fc0877036ebe5c23a06306e3e329fbbd6
emile-ross <emileross@tutanota.de>
2026-06-04 22:08:50 -0400

    feat: add command_parsing() function
    command_parsing() serves to filter out useless arguments

--- src/main.c
+++ src/main.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 
 void err(const char *error_message);
+int command_parsing(int num_args, char *arguments[]);
 
 int main(int argc, char *argv[])
 {
+	command_parsing(argc, argv);
 	if (!(argc > 1))
 	{
 		err("Missing arguments");
 	}
+
+
+	return 0; /* c90 return value */
 }
 
 
@@ -20,4 +25,19 @@ void err(const char *error_message)
 	{
 		exit(1);
 	}
+	exit(1);
+}
+
+int command_parsing(int num_args, char *arguments[])
+{
+	if (num_args == 2)
+	{
+		/* this means it is a simple command */
+		printf("%s\n", arguments[1]);
+	}
+	else
+	{
+		err("Invalid number of arguments");
+	}
+	return 0;
 }
