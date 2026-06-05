2669875d8f5665ffa31074719fd302c39ea0172d
emile-ross <emileross@tutanota.de>
2026-06-04 22:23:52 -0400

    feat: add program name in error message
     add error message memory allocation

--- src/main.c
+++ src/main.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+
+char *program_name = "medit";
 
 void err(const char *error_message);
 int command_parsing(int num_args, char *arguments[]);
 
 int main(int argc, char *argv[])
 {
-	command_parsing(argc, argv);
 	if (!(argc > 1))
 	{
-		err("Missing arguments");
+		char *error_message_template = "%s expects arguments";
+		size_t error_message_len = 1 + (size_t)snprintf(NULL, 0, error_message_template, program_name);
+		char *error_message = malloc(error_message_len);
+		snprintf(error_message, error_message_len, error_message_template, program_name);
+		err(error_message);
 	}
 
+	command_parsing(argc, argv);
 
 	return 0; /* c90 return value */
 }
