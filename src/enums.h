e057ccb59344c8aceb060db2fa9ca2f96fb7c75f
emile-ross <emileross@tutanota.de>
2026-06-05 18:14:44 -0400

    feat: add flag_type enum
    create the enums.h header file and include it from the header.h.  This commit also includes the string.h library since it will be needed for future command parsing

---
+++ src/enums.h
@@ -0,0 +1,8 @@
+enum flag_type
+{
+	base_flag,
+	total_time,
+	interval_time,
+	start_time,
+	cap_time,
+};
--- src/header.h
+++ src/header.h
@@ -1,4 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "enums.h"
 
 extern char *program_name;
