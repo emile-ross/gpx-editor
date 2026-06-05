d4ee5828db1593b1f5f36472abad146cac75d512
emile-ross <emileross@tutanota.de>
2026-06-05 18:20:56 -0400

    feat: add flag parsing and identification
    identify flags with their type stored in the flag_type_arr array at the
    index of the argument (in the command)

--- src/main.c
+++ src/main.c
@@ -35,12 +35,52 @@ void err(const char *error_message)
 	exit(1);
 }
 
+
 int command_parsing(int num_args, char *arguments[])
 {
+	enum flag_type flag_type_arr[num_args]; /* initialise flag_type_arr used to assign a type of flag to a command index */
+
+	int base_args = 1;	/* the index of the first possible valid argument (the command length "medit")*/
+	
+	for (int i = 0; i < base_args; i++)
+	{
+		flag_type_arr[i] = base_flag;
+	}
+
+
 	if (num_args == 2)
 	{
+		int base_arg_index = num_args - 1; /* different from useful_args_n because this is an index to the command */
+
+		/* checks if the argument is a valid flag 
+		 * loop through all arguments */
+		for (int i = base_args; i < base_arg_index; i++)
+		{
+			if (strcmp(arguments[i], "-t") == 0)
+			{
+				/* -t is total_time */
+				flag_type_arr[i] = total_time;
+			}
+			else if (strcmp(arguments[i], "-i") == 0)
+			{
+				flag_type_arr[i] = interval_time;
+			}
+			else if (strcmp(arguments[i], "-s") == 0)
+			{
+				flag_type_arr[i] = start_time;
+			}
+			else if (strcmp(arguments[i], "-c") == 0)
+			{
+				flag_type_arr[i] = cap_time;
+			}
+		}
+
 		/* this means it is a simple command */
 		printf("%s\n", arguments[1]);
+	}
+	else if (num_args == 4)
+	{
+
 	}
 	else
 	{
