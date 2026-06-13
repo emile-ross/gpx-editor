ddb3dd23c21a00c6fe183b2f665c495259258919
emile-ross <emileross@tutanota.de>
2026-06-13 15:30:44 -0400

    fix: invalid endptr comparaison
    dereference the the endptr pointer

--- src/time_parsing.c
+++ src/time_parsing.c
@@ -9,12 +9,15 @@ int time_parsing(bool *used_args[], int *flag_r_index, int num_args, char *argum
 	for (int i = flag_r; i < num_args; i++)
 	{
 		char *endptr = NULL;
+		bool valid_arg = false;
 
 		if (strcmp(argument[i], "-S") == 0)
 		{
 			*used_args[i] = true;	/* mark as used */
 			i++; /* read ahead */
+			*used_args[i] = true;	/* mark as used */
 			duration += (int)strtol(argument[i], &endptr, 10);
+			valid_arg = true;
 		}
 		else if (strcmp(argument[i], "-M") == 0)
 		{
@@ -22,6 +25,7 @@ int time_parsing(bool *used_args[], int *flag_r_index, int num_args, char *argum
 			i++; /* read ahead */
 			int minutes = (int)strtol(argument[i], &endptr, 10);
 			duration += (minutes * 60);
+			valid_arg = true;
 		}
 		else if (strcmp(argument[i], "-H") == 0)
 		{
@@ -29,11 +33,14 @@ int time_parsing(bool *used_args[], int *flag_r_index, int num_args, char *argum
 			i++; /* read ahead */
 			int hours = (int)strtol(argument[i], &endptr, 10);
 			duration += (hours * 3600);
+			valid_arg = true;
 		}
-
-		if (endptr != NULL)
+		if (valid_arg)
 		{
-			printf("error converting \"%s\" to a number\n", endptr);
+			if (*endptr != '\0')
+			{
+				printf("error converting \"%s\" to a number\n", endptr);
+			}
 		}
 	}
 
