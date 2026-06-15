56e3bf1b22f88ec5963713e5abcc1eb66638f9e6
emile-ross <emileross@tutanota.de>
2026-06-14 21:11:02 -0400

    feat: print default time
    this also gets the current time in order to set it as the default time

--- src/menu.c
+++ src/menu.c
@@ -1,23 +1,21 @@
+#include "header.h"
 
 void date_input(int *year, int *month, int *day, int *hour, int *minute, int *second)
 {
-
-
 	bool first_input = true;
 
-	const char date_template = "%d-%d-%d %d:%d:%d";
+	time_t default_time = time(NULL);
+
+	struct tm *final_time = localtime(&default_time);
 
 	if (first_input)
 	{
-
-		size_t default_time_size = 1 + snprintf(NULL, 0, date_template, );
-		date_template
-
-
-		printf("Default date is ");
+		printf("The time is set to the current time by default\n");
 	}
-	else
-	{
-		printf("Current date is %d-%d-%d %d:%d:%d");
-	}
+
+	char *time_string = malloc((size_t)max_time_len);
+	strftime(time_string, (size_t)max_time_len, "%Y-%m-%d %H:%M:%S", final_time);
+	
+	printf("Date: %s\n", time_string);
+
 }
