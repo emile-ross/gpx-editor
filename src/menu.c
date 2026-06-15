b17794ee6d11cacd055e3764c0dd34b693ca51d5
emile-ross <emileross@tutanota.de>
2026-06-14 20:55:02 -0400

    feat: add menu.c
    used for getting the start time or time end

---
+++ src/menu.c
@@ -0,0 +1,23 @@
+
+void date_input(int *year, int *month, int *day, int *hour, int *minute, int *second)
+{
+
+
+	bool first_input = true;
+
+	const char date_template = "%d-%d-%d %d:%d:%d";
+
+	if (first_input)
+	{
+
+		size_t default_time_size = 1 + snprintf(NULL, 0, date_template, );
+		date_template
+
+
+		printf("Default date is ");
+	}
+	else
+	{
+		printf("Current date is %d-%d-%d %d:%d:%d");
+	}
+}
