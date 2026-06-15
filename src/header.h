4b1c33f00eaf4434431f09d15e82cec6377e2e73
emile-ross <emileross@tutanota.de>
2026-06-14 21:10:08 -0400

    build: add menu.c file
    compile with the menu.c file

--- src/header.h
+++ src/header.h
@@ -1,9 +1,12 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
+#include <time.h>
 
 #include "enums.h"
+
+#define max_time_len 32
 
 /* global variables */
 	extern const char *program_name;
@@ -14,3 +17,5 @@ int command_parsing(int num_args, char *arguments[]);
 int time_parsing(int *flag_r_index, int num_args, char *argument[]);
 
 void stotime(int total_seconds, int *hours, int *minutes, int *seconds);
+
+void date_input(int *year, int *month, int *day, int *hour, int *minute, int *second);
