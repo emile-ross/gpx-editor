4e1e8bee68a7ad1f95bbfc193811dc6ec2655186
emile-ross <emileross@tutanota.de>
2026-06-15 22:28:38 -0400

    refactor: move wait() from menu.c to functions.c
    title is self explanatory

--- src/functions.c
+++ src/functions.c
@@ -18,3 +18,25 @@ void clear(void)
 	/* This ANSI sequence clears the screen */
 	printf("\033[2J\033[H");
 }
+
+void wait(int quarters_seconds, int num_seconds)
+{
+	struct timespec install_timer;
+	long int time_timer_quarters;
+	long int time_timer_seconds;
+
+	if (quarters_seconds < 4)	/* 4 quarters per second */
+	{
+		time_timer_quarters = quarters_seconds * 250000000;	/* convert quarters to nanoseconds */
+		time_timer_seconds = num_seconds;			/* set seconds */
+	}
+	else
+	{
+		time_timer_quarters = 0;
+		time_timer_seconds = num_seconds + 1;	/* adds 1 second if 4 >= quarters */
+	}
+
+	install_timer.tv_nsec = time_timer_quarters;
+    	install_timer.tv_sec = time_timer_seconds;
+    	nanosleep(&install_timer, NULL);
+}
