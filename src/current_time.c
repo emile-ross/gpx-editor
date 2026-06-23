8981a1565a29a75653bb3695e6b589c861eb7d70
emile-ross <emileross@tutanota.de>
2026-06-22 23:59:46 -0400

    feat: add time in current time
    add times like seconds, minutes and hours

--- src/current_time.c
+++ src/current_time.c
@@ -8,6 +8,10 @@ struct maptime current_time(void)
 	struct maptime current_time_m;
 	
 	current_time_m.year = cur_time.year;
+	current_time_m.month = cur_time.month;
+	current_time_m.day = cur_time.mday;
+	current_time_m.minute = cur_time.min;
+	current_time_m.second = cur_time.sec;
 
 	return current_time_m;
 }
