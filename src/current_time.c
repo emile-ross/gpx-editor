fbbfb4050bfbc9559126fe88ea8094656700417a
emile-ross <emileross@tutanota.de>
2026-06-24 14:17:55 -0400

    feat: initialise time struct
    intialise it using a new function time_initialisation()

--- src/current_time.c
+++ src/current_time.c
@@ -10,12 +10,14 @@ bool in_bounds(int input)
 	return true;
 }
 
+void time_initialisation(struct maptime* initial_time);
+
 struct maptime current_time(void)
 {
 	time_t default_time = time(NULL);
 	struct tm *cur_time = localtime(&default_time);
-
 	struct maptime current_time_m;
+	time_initialisation(&current_time_m);
 	
 	/* get current year + 1900 because tm struct stores the year from 1900 */
 	int y = cur_time->tm_year;
@@ -29,4 +31,13 @@ struct maptime current_time(void)
 	current_time_m.minute = (uint8_t)cur_time->tm_min;
 	current_time_m.second = (uint8_t)cur_time->tm_sec;
 	return current_time_m;
+}
+
+void time_initialisation(struct maptime* initial_time)
+{
+	initial_time->month = 255;
+	initial_time->day = 255;
+	initial_time->hour = 255;
+	initial_time->minute = 255;
+	initial_time->second = 255;
 }
