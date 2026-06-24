532c74b661345f018cd886337000cadfe6e46335
emile-ross <emileross@tutanota.de>
2026-06-24 03:40:02 -0400

    fix: invalid casts and conversion
    narrowing conversions are dangerous and this commit aims to remove a few
    of them
    many can be seen when converting unsigned longs to ints
    this narrows down the width from 64bit to 32bit (losing half of the
    bits)

--- src/functions.c
+++ src/functions.c
@@ -1,15 +1,15 @@
 #include "header.h"
 
-void stotime(int total_seconds, int *hours, int *minutes, int *seconds)
+void stotime(unsigned long total_seconds, uint8_t *hours, uint8_t *minutes, uint8_t *seconds)
 {
-	int seconds_remainder = total_seconds % 60;
+	unsigned long seconds_remainder = total_seconds % 60;
 	*seconds = seconds_remainder;
 
-	int minutes_pre_remainder = total_seconds - seconds_remainder;
-	int minutes_remainder = minutes_pre_remainder % 3600;
+	unsigned long minutes_pre_remainder = total_seconds - seconds_remainder;
+	unsigned long minutes_remainder = minutes_pre_remainder % 3600;
 	*minutes = minutes_remainder / 60;
 
-	int hours_temp = minutes_pre_remainder - minutes_remainder;
+	unsigned long hours_temp = minutes_pre_remainder - minutes_remainder;
 	*hours = hours_temp / 3600;
 }
 
--- src/header.h
+++ src/header.h
@@ -33,11 +33,12 @@ int command_parsing(int num_args, char *arguments[]);
 long time_parsing(int *flag_r_index, int num_args, char *argument[]);
 int conversion_check(const char *endptr, const char *input_string, const bool newline);
 
-void stotime(int total_seconds, int *hours, int *minutes, int *seconds);
+void stotime(unsigned long total_seconds, uint8_t *hours, uint8_t *minutes, uint8_t *seconds);
 
-struct maptime* date_input(void);
 uint16_t get_time(uint16_t lower_bound, uint16_t upper_bound, char *date_type, bool eight_bit_conversion);
 
 void wait(long quarters, long seconds);	/* waits for a certain time */
 
 size_t timetotext(char *target, struct maptime *date);
+struct maptime current_time(void);
+struct maptime date_input(void);
