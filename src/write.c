0adf65482d0cb795dcac975c965a6deb597a140a
emile-ross <emileross@tutanota.de>
2026-06-24 23:17:32 -0400

    feat: add seconds structure in forwards_write()
    this will be used for incrementing the seconds count

--- src/write.c
+++ src/write.c
@@ -1,13 +1,27 @@
 #include "header.h"
 
-void forwards_write(struct maptime *track_time)
+void forwards_write(struct maptime *track_time, int num_waypoints, const unsigned long time_interval)
 {
-	uint16_t year = track_time->year;
-	uint8_t month = track_time->month;
-	uint8_t day = track_time->day;
-	uint8_t hour = track_time->hour;
-	uint8_t minute = track_time->minute;
-	uint8_t second = track_time->second;
+	track_time->year;
+	track_time->month;
+	track_time->day;
+	track_time->hour;
+	track_time->second;
 
+	for (int i = 0; i < num_waypoints; i++);
+	{
+		unsigned long interval = time_interval;
+
+		uint8_t seconds = track_time->second + interval % 60; 
+
+		if (seconds >= 60)
+		{
+			track_time->minute++;
+			seconds -= 60;
+		}
+		interval -= interval % 60;
+
+		track_time->second = seconds;
+	}
 	const char *track_time_template = "<when>%u-%u-%uT%u:%u:%uZ</when>";
 }
