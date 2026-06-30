441a8b7acab915ab1d4fd9456b34eded43c73fb2
emile-ross <emileross@tutanota.de>
2026-06-30 16:32:04 -0400

    fix: add bounds checking to date input
    this only gets the time if the choice is valid

--- src/menu.c
+++ src/menu.c
@@ -55,48 +55,50 @@ struct maptime date_input(void)
 				valid_choice = true;
 			}
 
-			uint16_t date = 0;
-			switch (choice)
-			{
-				case 1:
-					date = get_time(0, 65535, "year", false);
-					final_time.year = date;
-					break;
-
-				case 2:
-					date = get_time(1, 12, "months", true);
-					final_time.month = (uint8_t)date;
-					break;
-					
-				case 3:
-					date = get_time(1, 31, "days", true);
-					final_time.day = (uint8_t)date;
-					break;
-
-				case 4:
-					date = get_time(0, 23, "hours", true);
-					final_time.hour = (uint8_t)date;
-					break;
-
-				case 5:
-					date = get_time(0, 59, "minutes", true);
-					final_time.minute = (uint8_t)date;
-					break;
-
-				case 6:
-					date = get_time(0, 59, "seconds", true);
-					final_time.second = (uint8_t)date;
-					break;
-
-				default:
-					valid_choice = false;
-					break;
-			}
-			printf("%d\n", date);
-
 			if (valid_choice)
 			{
-				first_input = false;
+				uint16_t date = 0;
+				switch (choice)
+				{
+					case 1:
+						date = get_time(0, 65535, "year", false);
+						final_time.year = date;
+						break;
+
+					case 2:
+						date = get_time(1, 12, "months", true);
+						final_time.month = (uint8_t)date;
+						break;
+						
+					case 3:
+						date = get_time(1, 31, "days", true);
+						final_time.day = (uint8_t)date;
+						break;
+
+					case 4:
+						date = get_time(0, 23, "hours", true);
+						final_time.hour = (uint8_t)date;
+						break;
+
+					case 5:
+						date = get_time(0, 59, "minutes", true);
+						final_time.minute = (uint8_t)date;
+						break;
+
+					case 6:
+						date = get_time(0, 59, "seconds", true);
+						final_time.second = (uint8_t)date;
+						break;
+
+					default:
+						valid_choice = false;
+						break;
+				}
+
+				if (valid_choice)
+				{
+					first_input = false;
+				}
 			}
 		}
 	}
