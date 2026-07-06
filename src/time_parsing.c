8a0eb84ad17c7a8022348f54e0378ff9cc3d93f1
emile-ross <emileross@tutanota.de>
2026-07-06 02:57:20 -0400

    fix: segfault when missing arguments
    this adds error checking in order to prevent invalid conversion of an
    invalid (not existing) argument

--- src/time_parsing.c
+++ src/time_parsing.c
@@ -15,7 +15,14 @@ uint32_t time_parsing(int *flag_r_index, int num_args, char *argument[])
 
 	for (int i = flag_r; i < num_args; i++)
 	{
+		int next_index = i + 1;
+		if (!(next_index < num_args))
+		{
+			err("Missing flags in time parsing\nUse flags for specifying the time \"-S\", \"-M\" or \"-H\"");
+			break;
+		}
 		char *endptr = NULL;
+		long date_arg = strtol(argument[next_index], &endptr, 10);
 		bool valid_arg = false;
 
 		if (strcmp(argument[i], "-S") == 0)
@@ -23,49 +30,46 @@ uint32_t time_parsing(int *flag_r_index, int num_args, char *argument[])
 			i++; /* read ahead */
 
 			/* no need to convert since this is already in seconds */
-			long seconds = strtol(argument[i], &endptr, 10);
 			if (conversion_check(endptr, argument[i], false))
 				valid_arg = true;
 			else
 				parsing_fail("seconds");
 
 			
-			if (bound_check(seconds, 0, 59))
+			if (bound_check(date_arg, 0, 59))
 			{
 				num_valid_args++;
-				duration += (uint32_t)seconds;
+				duration += (uint32_t)date_arg;
 			}
 		}
 		else if (strcmp(argument[i], "-M") == 0)
 		{
 			i++; /* read ahead */
 			/* convert minutes to seconds */
-			long minutes = strtol(argument[i], &endptr, 10);
 			if (conversion_check(endptr, argument[i], false))
 				valid_arg = true;
 			else
 				parsing_fail("minutes");
 
-			if (bound_check(minutes, 0, 59))
+			if (bound_check(date_arg, 0, 59))
 			{
 				num_valid_args++;
-				duration += (uint32_t)(60 * minutes);
+				duration += (uint32_t)(60 * date_arg);
 			}
 		}
 		else if (strcmp(argument[i], "-H") == 0)
 		{
 			i++; /* read ahead */
 			/* convert hours to seconds */
-			long hours = strtol(argument[i], &endptr, 10);
 			if (conversion_check(endptr, argument[i], false))
 				valid_arg = true;
 			else
 				parsing_fail("hours");
 
-			if (bound_check((3600 * hours), 0, UINT32MAX))
+			if (bound_check((3600 * date_arg), 0, UINT32MAX))
 			{
 				num_valid_args++;
-				duration += (uint32_t)(3600 * hours);
+				duration += (uint32_t)(3600 * date_arg);
 			}
 		}
 
