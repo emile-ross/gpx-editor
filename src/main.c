fcf6545363fdd20e56a1f995e9c7f88586d41283
emile-ross <emileross@tutanota.de>
2026-07-06 01:39:43 -0400

    fix: stack overflow in err() & add err checking
    this fixes the recursion in err() when calling err() again if malloc
    fails it will fail recursively and this will cause a stack overflow due
    to excessive recursion
    
    this also frees buffers before exiting the program in order to prevent
    any possible memory leak

--- src/main.c
+++ src/main.c
@@ -30,27 +30,38 @@ void err(const char *restrict format, ...)
 	/* check for a potential malloc() fail */
 	if (base_error_msg == NULL)
 	{
-		err("malloc() failed to allocate memory");
+		fprintf(stderr, "%s: malloc() failed to allocate memory", program_name);
+		exit(1);
 	}
 	/* write to buffer & compare return value with msg size calculated previously */
 	size_t ret = (size_t)vsnprintf(base_error_msg, msg_body_size, format, args);
 	va_end(args);
 
-	if (ret > msg_size)
+	if (ret > msg_body_size)
+	{
+		fprintf(stderr, "%s: Error message write failed", program_name);
+		free(base_error_msg);
+		exit(1);
+	}
 	const char *message_template = "%s: %s";	/* program_name then the message */
 
 	size_t final_msg_size = 1 + (size_t)snprintf(NULL, 0, message_template, program_name, base_error_msg);
 	char *error_msg = malloc(final_msg_size);
 	if (error_msg == NULL)
 	{
-		err("malloc() failed to allocate memory");
+		free(base_error_msg);
+		fprintf(stderr, "%s: malloc() failed to allocate memory", program_name);
 	}
 	/* write to buffer & compare return value with msg size calculated previously */
 	ret = (size_t)snprintf(error_msg, final_msg_size, message_template, program_name, base_error_msg);
 	free(base_error_msg);
 
-
-	/* calculate message length */
+	if (ret > final_msg_size)
+	{
+		fprintf(stderr, "%s: Error message write failed", program_name);
+		free(error_msg);
+		exit(1);
+	}
 
 	fprintf(stderr, "\x1b[31m%s\x1B[0m\n", error_msg);
 	free(error_msg);
