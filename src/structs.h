818065dc8ea66c1485c911532dd152028b184839
emileross <emileross@tutanota.de>
2026-07-21 20:01:52 -0400

    refactor: remove useless struct definition
    not used anywhere

--- src/structs.h
+++ src/structs.h
@@ -8,13 +8,6 @@ typedef struct maptime
 	uint8_t second;
 } MapTime;
 
-typedef struct
-{
-	Bool interval_specified;
-	Bool start_time_specified;
-	Bool end_time_specified;
-}  output_arguments_bl;
-
 struct maptime current_time(void);
 struct maptime date_input(void);
 
